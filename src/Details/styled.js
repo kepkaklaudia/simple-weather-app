640a6661ffbe36f15fd094fe99746da9785c8df2
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-08 00:32:35 +0100

    grid and background changed

--- src/Details/styled.js
+++ src/Details/styled.js
@@ -1,24 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-display: grid;
-grid-template-columns: repeat(3, 150px);
-grid-gap: 20px;
-margin: 0 auto;
-padding: 80px 20px;
-justify-content: center;
   display: grid;
   grid-template-columns: repeat(3, 150px);
   grid-gap: 20px;
   margin: 0 auto;
-  padding: 80px 20px;
+  padding: 20px 20px 80px 20px;
   justify-content: center;
-@media (max-width: 600px) {
-  grid-template-columns: repeat(2, 150px);
-}
-@media (max-width: 380px) {
-  grid-template-columns: repeat(1, 150px);
-}
+
   @media (max-width: 600px) {
     grid-template-columns: repeat(2, 150px);
   }
@@ -29,13 +18,10 @@ justify-content: center;
 `;
 
 export const Detail = styled.div`
-background-color: yellow;
-border-radius: 20px;
-padding: 5px;
-  background-color: yellow;
+  background-color: rgba(255, 255, 255, 0.3);
   border-radius: 20px;
 `;
 
 export const Text = styled.p`
-  margin: 15px;
+  margin: 10px;
 `;
