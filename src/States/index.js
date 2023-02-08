904ec6e94813804c2bd2013df9135eb217a7cf7f
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-08 14:39:54 +0100

    paths changed

--- src/States/index.js
+++ src/States/index.js
@@ -5,7 +5,7 @@ export const State = ({ info, additional, alt, src, }) => (
     <Wrapper>
       <Details>
         <p>{info}</p>
-        <Image alt={alt} src={`icons/${src}.png`} />
+        <Image alt={alt} src={`${process.env.PUBLIC_URL}/icons/${src}.png`} />
         {additional}
       </Details>
     </Wrapper>
